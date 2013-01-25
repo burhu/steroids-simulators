675a36a3fd436e82d810f962b1f2b8190c07391b
Juha Suuraho <juha.suuraho@appgyver.com>
2013-01-24 16:28:02 -0800

    better bin/publish.sh script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,2 +1,4 @@
-tar cvzf steroids-simulators.tar.gz *
+npm pack
+mv steroids-simulators* steroids-simulators.tar.gz
 s3cmd put --acl-public --guess-mime-type steroids-simulators.tar.gz s3://appgyver.production.steroids.simulators/
+rm steroids-simulators.tar.gz
